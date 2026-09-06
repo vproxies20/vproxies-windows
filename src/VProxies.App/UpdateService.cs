297a7644e671df30f59c7c595047b1ce09195175
vproxies20 <app@vproxies.app>
2026-09-07 04:22:43 +0700

    Add explicit updater framework namespaces

--- src/VProxies.App/UpdateService.cs
+++ src/VProxies.App/UpdateService.cs
@@ -1,9 +1,16 @@
-using System.Net.Http.Headers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Reflection;
 using System.Security.Cryptography;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace VProxies;
 
